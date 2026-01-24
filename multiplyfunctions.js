bf19724eb5a71c853c0c791737059807aec8fd0d
soarin17 <sriadibhatla1@gmail.com>
2026-01-24 12:01:36 +0100

    finished multiplying with functions

---
+++ multiplyfunctions.js
@@ -0,0 +1,17 @@
+const number1 = (15);
+const number2 = (3);
+
+function Multiplication(number1, number2){
+    const number = Number(number1)
+    const number3 = Number(number2)
+    if (Number.isNaN(number)) {
+        console.log("THIS IS NOT A NUMBER. RUN THIS PROGRAM AND TRY AGAIN")
+        process.exit(1)
+    };
+    if (Number.isNaN(number3)) {
+      console.log("THIS IS NOT A NUMBER. RUN THIS PROGRAM AND TRY AGAIN")
+      process.exit(1)
+    };
+    console.log(number1*number2)
+};
+Multiplication(2,4)
