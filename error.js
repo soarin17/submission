07dc2bec76be990f862b47ddb2a2f88eee8b15db
soarin17 <sriadibhatla1@gmail.com>
2026-02-28 15:23:24 +0100

    done with error project

---
+++ error.js
@@ -0,0 +1,8 @@
+try {
+    let name = "karthik";
+    console.log(jam);
+} catch (error) {
+    let name = "karthik"
+    console.log("there was an error, here is the correct code")
+    console.log(name)
+}
